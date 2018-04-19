3d02798213a44c5b4f3ef0c24f3a7d02c2705131
nicolas roa <nrrodriguez2011@hotmail.com>
2018-04-19 11:22:40 -0500

    app

---
+++ app/views.py
@@ -0,0 +1,42 @@
+from post import postfb, ite_groups
+from flask import request, redirect, render_template
+from app import app
+
+def users(tokens):
+	usuarios={}
+	usuario = 0
+	tokens = tokens.split(',')
+	for user in tokens:
+		if user!= '' :
+			usuario = usuario+1
+			usuarios['usuario'+ str(usuario)] ={'token': user ,'numeropost':0}
+	return usuarios
+
+def IDgroups(grupos):
+    groups = []
+    lis_grupos = grupos.split(',')
+    for group in lis_grupos:
+        if group != '':
+            groups.append(group)    
+    return groups
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template(
+        'index.html'
+    )
+
+@app.route('/', methods=['POST'])
+def params():   
+    message = request.form['post']
+    tokens = request.form['token']
+    grupos = request.form['grupos']
+    usuarios = users(tokens)
+    groups = IDgroups(grupos)
+    print(len(message))
+    if len(groups) >= 1 and len(usuarios) >= 1 and len(message) != 0:
+        ite_groups(groups,message,usuarios)
+        valor1 = 'post exitoso'
+    else:
+        valor1 = 'faltan parametros'
+    return render_template('index.html', valor1 = valor1)
