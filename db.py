7cda8c9c76c900483d887f197e100ab5bc189e64
nicolas roa <nrrodriguez2011@hotmail.com>
2018-04-26 17:10:13 -0500

    update

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 engine = create_engine('postgres://fhpebriezsuyly:576364a25eed0956440777de9005f84bd021b89d5f65817338be42fc19df35de@ec2-23-23-180-121.compute-1.amazonaws.com:5432/d6b8r5d8bu18ri')
 metadata = MetaData()
 formulario = Table('formulario', metadata,
-    Column('post', String, primary_key=True),
+    Column('post', String),
     Column('tokens', String),
     Column('groups', String),
     Column('notification', String)
